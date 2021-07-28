58eec880593b861705dfdce782d1398f60510855
danielboots <you@example.com>
2021-07-28 13:29:11 +0100

    updated service template

--- pages/service/[slug].js
+++ pages/service/[slug].js
@@ -1,9 +1,12 @@
 import { sanityClient, urlFor } from "../../sanity";
 import Layout from "@/components/Layout";
 import Link from "next/link";
+import BlockContent from "@sanity/block-content-to-react";
+import Info from "@/components/Info";
 
 const Service = ({
   title,
+  body,
   slug,
   tagline,
   id,
@@ -15,35 +18,56 @@ const Service = ({
     <Layout title="About | EDM" description="About| EDM Music Services">
       <div>
         <main>
-          <article className="font-body  shadow-lg mx-auto ">
-            <header className="">
-              <div className=" h-full w-full flex items-center justify-center p-8">
-                <div className="bg-white rounded p-6">
-                  <h1 className="text-4xl mb-4 flex justify-center font-black  text-gray-900 tracking-tight uppercase">
-                    {title}
-                  </h1>
-                  <div>
-                    <img
-                      className="w-full h-32 sm:h-48 object-cover  shadow-lg  hover:shadow-2xl transition duration-300 ease-in-out relative border-1"
-                      src={mainImage.asset.url}
-                      alt="alt tag"
-                    />
+          <div>
+            {/* Main container div */}
+
+            <div className="absolute h-full w-full flex overflow-x-auto bg-coolgray-900 bg-opacity-50 backdrop-filter  "></div>
+            <h1 className=" text-white z-20 uppercase font-body text-center font-bold   text-3xl  sm:text-4xl md:text-5xl ">
+              {title}
+            </h1>
+          </div>
+
+          <div className="   container mx-auto text-lg text-gray-500 text-justify grid  grid-cols-1 p-4  sm:grid-cols-2">
+            <div className="  flex items-center ">
+              <img
+                src={mainImage.asset.url}
+                className="hover:shadow-2xl transition duration-300 ease-in-out relative border-1"
+                alt=""
+              />
+            </div>
+            <div className=" px-4 mb-6 flex font-body">
+              <div className=" pt-6">
+                <div className=" text-3xl  justify-center font-black  text-gray-700 pb-4">
+                  {title}...
+                  <div className=" text-sm text-gray-400 tracking-widest">
+                    {tagline}.
                   </div>
-                  <div className="prose  text-center my-10 ">{description}</div>
-                  <Link href="/contact">
-                    <button
-                      type="button"
-                      className=" m-3 py-3 px-4  uppercase  text-xs  rounded-sm font-bold  text-white bg-gray-900 hover:bg-gray-800 justify-end"
-                    >
-                      Enquire ...
-                    </button>
-                  </Link>
                 </div>
+                {description}
               </div>
-            </header>
-          </article>
+            </div>
+          </div>
+
+          <div className="container mx-auto ">
+            <div className=" text-lg font-body text-gray-500 m-4 text-justify  leading-relaxed ">
+              <BlockContent
+                blocks={body}
+                projectId="ta2muy7p"
+                dataset="production"
+              />
+            </div>
+            <Link href="/contact">
+              <button
+                type="button"
+                className=" m-3 py-3 px-4  uppercase  text-xs  rounded-sm font-bold  text-white bg-gray-900 hover:bg-gray-800 justify-end"
+              >
+                Enquire ...
+              </button>
+            </Link>
+          </div>
         </main>
       </div>
+      <Info />
     </Layout>
   );
 };
@@ -54,6 +78,7 @@ export const getServerSideProps = async (pageContext) => {
 
   const query = `*[_type == 'service' && slug.current == $pageSlug][0]{
     title,
+    body,
     slug,
     tagline,
     id,
@@ -89,6 +114,7 @@ export const getServerSideProps = async (pageContext) => {
         mainImage: service.mainImage,
         description: service.description,
         tags: service.tags,
+        body: service.body,
       },
     };
   }
